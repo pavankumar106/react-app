3e064c28ffb89b3e1f7d978f77e5a9bf17e6ead8
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-08-23 19:40:12 +0530

    commits

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,18 +1,10 @@
 import React from "react";
-
+import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   console.log(props);
   const renderContactList = props.contacts.map((contacts) => {
-    return (
-      <div className="item">
-        <div className="content">
-          <div className="header">{contacts.name}</div>
-          <div>{contacts.email}</div>
-        </div>
-        <i className="trash alternate outline icon"></i>
-      </div>
-    );
+    return <ContactCard contacts={contacts} />;
   });
   return <div className="ui celled list">{renderContactList}</div>;
 };
-export default ContactList;
+export default ContactCard;
